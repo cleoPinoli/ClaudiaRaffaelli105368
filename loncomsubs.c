ba5d1b793e441977cc392a040c1f55b9e11c8db3
cleoPinoli <claudia.raffaelli@studenti.unicam.it>
2021-12-20 15:41:34 +0100

    First steps
    someone has once told me, "You should drink serotonin in small sips."
    This is today's small sip of serotonin for me:
    configured the CMakeLists.txt;
    created a cpl source files (two, to be precise);
    successfully opened a file from project folder;
    
    This is it for the day, besides I should do this on the Linux partition, while atm I'm under Windows. No bueno.
    *small sip* i have a lecture anyway, gtg.  I'll dig deeper this evening.

---
+++ loncomsubs.c
@@ -0,0 +1,12 @@
+#include <fcntl.h> /* file control options */
+#include <stdio.h>
+
+void my_fread () {
+    int my_fd;
+    my_fd = fopen("test.txt", O_RDONLY);
+    if (my_fd < 0) {
+        perror("Error: ");
+    } else {
+        printf("File has been opened! Great news, innit?");
+    }
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/* stdlib contains declaration of malloc and free, will implement elsewhere */
+
+void main () {
+    my_fread();
+    printf("\nHello world!");
+}
